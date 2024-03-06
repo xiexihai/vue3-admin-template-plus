e1475d448e5125e258142cdaa70a4e5285912fb4
xiexihai <710961684@qq.com>
2024-03-06 23:29:48 +0800

    feat: build

---
+++ dist/assets/AccountView-CvPM7woo.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,e as n}from"./index-DAFlAsVg.js";const o={};function t(r,s){return c(),n("div",null,"账户设置")}const a=e(o,[["render",t]]);export{a as default};
--- dist/assets/MenusView-CMYI9OQG.js
+++
@@ -1,1 +0,0 @@
-import{l as e,o as c,c as n}from"./index-BW68TRW4.js";const o={};function r(t,s){return c(),n("div",null,"菜单管理")}const _=e(o,[["render",r]]);export{_ as default};
